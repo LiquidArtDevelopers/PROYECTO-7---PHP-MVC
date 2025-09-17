1454a9cb96a0ae3554e66a8cf90fd68724360dff
liquidartdevelopers <aranaz@gmail.com>
2025-09-17 19:48:26 +0200

    clase 44 anotaciones

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once "./php/config/helpers.php";
 // A CADA RUTA PERMITIDA LE ASIGNAMOS LA UBICACIÓN DEL ARCHIVO QUE TIENE SU CONTENIDO
 
 // Array de idiomas permitidos
-$langs = ['es', 'eu', 'en'];
+$langs = ['es', 'eu', 'en', 'fr'];
 
 //Array asociativo (3 nieveles) para determinar las url permitidas y asociarles el recurso de la vista que corresponda
 $arrayRutasGet = [
@@ -52,18 +52,38 @@ $arrayRutasGet = [
         '/en/contact' => [
             'view'      => '/php/views/en/contacto.php'
         ]
+    ],
+    'fr' => [
+        '/fr' => [
+            'view'      => '/php/views/fr/inicio.php'
+        ],
+        '/fr/a-propos-de-nous' => [
+            'view'      => '/php/views/fr/quienesSomos.php'
+        ],
+        '/fr/services' => [
+            'view'      => '/php/views/fr/servicios.php'
+        ],
+        '/fr/contact' => [
+            'view'      => '/php/views/fr/contacto.php'
+        ]
     ]
+
 ];
 
-
 // FASE 2 - COMO TODAS LAS PETICIONES LLEGAN AL INDEX.PHP (POR ACCIÓN DEL HTACCESS), AQUÍ ANALIZAMOS LA URL POR LA QUE VIENE PARA VER SI ES VÁLIDA O NO Y EN CASO DE SER VÁLIDA, CARGARLE LA VISTA (VIEW) ASIGNADA A ESA URL EN EL ARRAY.
+// EN ESTA FASE 2 ANALIZAMOS LA URL POR LA QUE EL USUARIO VIENE, Y EXTRAEMOS EL LENGUAJE EN $lang Y LA RUTA AMIGABLE EN $url, SIENDO ESTAS DOS VARIABLES NECESARIAS PARA LA FASE 3
+
 
 //Obtenemos la url entera desde la raiz
 // Ejemplo: "/es/contacto?id=10"
-$request = urldecode($_SERVER["REQUEST_URI"]) ?? "/es";
+$request = urldecode($_SERVER["REQUEST_URI"]) ?? '/es';
+
+
+
 // Extraemos únicamente el path para ignorar los parámetros de consulta
 // ejemplo: "/es/contacto"
 $url = parse_url($request, PHP_URL_PATH) ?? "/es";
+
 
 // Compruebo que $url no sea un "/", sino que sea otras url como "/es/contacto"
 if ($url != "/") {
@@ -72,6 +92,7 @@ if ($url != "/") {
     $url = rtrim($url, "/");
     // De $url me divide y extrae todos los valores teniendo la "/" como divisor.
     // Ejemplo: en $urlParse tengo un array con estos valores ["", "es", "contacto"] ya que $url es "/es/contacto"
+    // Ejemplo de explode a una $url que es "/es/productos/taza/amarilla" ["", "es", "productos", "taza", "amarilla"]
     // Ejemplo si fuese $url "/es", entonces el array sería ["", "es"]
     $urlParse = explode("/", $url);
     // Metemos dentro de $lang, el valor del array enb la posición 1, que es donde está el idioma. El arrat cuenta sus items empezando desde el 0, por lo tanto en el array ["", "es", "contacto"], la posición 1 es "es"
@@ -83,7 +104,6 @@ if ($url != "/") {
     header("Location: /es");
     exit;
 }
-
 
 // Una barrera para contener que si el usuario ha metido a mano o la url viene con un $lang que no se contempla en el array, le redirijamos a la página de inicio, pero con el idioma que queramos por defecto,
 if (!in_array($lang, $langs)) {
@@ -99,9 +119,12 @@ if (isset($arrayRutasGet[$lang][$url])) {
 
     // Si la url existe dentro del array de url's, entonces cogemos el valor de view, que es el archivo que haremos include para cargar el contenido pertienente de esta url.
     $view = $arrayRutasGet[$lang][$url]['view']; 
+
+    
     
     //----VISTA----------
     require_once  __DIR__ . $view;
+    
 } else {
 
     // En caso de que $url no exista dentro del array de url permitidas, cargamos el contenido de la 404
--- php/views/eu/inicio.php
+++ php/views/eu/inicio.php
@@ -1,11 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/inicio.min.css">
 </head>
 <body>
-    <h1>Hasiera horria</h1>
+    <!-- HERO01 -->
+    <header>        
+        <h1>3 Proiektua</h1>
+        <div>            
+            <img src="https://dummyimage.com/500x300" alt="">
+        </div>
+        <a href="" class="boton">CTA</a>    
+    </header>
 </body>
 </html>
